c5ec0c2e496af8653c3fecf84a8f52b60a5a7331
dimitroffangel <angeldimitrov05@gmail.com>
2022-10-13 16:07:45 +0300

    added a way to resolve the interpenetration of two colliding objects

--- Physics/Systems/CollisionHelper.h
+++ Physics/Systems/CollisionHelper.h
@@ -9,41 +9,33 @@ namespace Aguamenti
 {
 	const Real RESTITUTION_DEFAULT = -0.75;
 
-	void ResolveContact(const Real deltaTime, ParticleComponent* lhsParticleComponent, 
-		ParticleComponent* rhsParticleComponent, const Vector3 contactNormal, const Real restitution = RESTITUTION_DEFAULT)
+	void ResolveContact(const Real deltaTime, ParticleComponent* lhsParticleComponent,
+		ParticleComponent* rhsParticleComponent, const Vector3 contactNormal, const Real restitution = RESTITUTION_DEFAULT);
+	void ResolveInterpenetration(const Real deltaTime,
+		ParticleComponent* lhsParticleComponent, ParticleComponent* rhsParticleComponent, const Real penetrationLength,
+		const Vector3 contactNormal)
 	{
+		if (penetrationLength <= 0)
+		{
+			return;
+		}
 		if (lhsParticleComponent == nullptr)
 		{
+			assert(false && "Aguamenti::ResolveInterpenetration lhsParticleComponent does not exist");
 			return;
 		}
 
-		Vector3 deltaVelocityBetweenParticleComponents = lhsParticleComponent->m_Velocity;
-		if (rhsParticleComponent != nullptr)
-		{
-			deltaVelocityBetweenParticleComponents -= rhsParticleComponent->m_Velocity;
-		}
-		const Real seperatingVelocity = deltaVelocityBetweenParticleComponents * contactNormal;
-		if (seperatingVelocity > 0)
-		{
-			return;
-		}
-		const Real newSeperatingVelocity = -seperatingVelocity * restitution;
-		const Real deltaSeperatingVelocity = newSeperatingVelocity - seperatingVelocity;
 		Real totalInverseMass = lhsParticleComponent->m_InverseMass;
 		if (rhsParticleComponent != nullptr)
 		{
 			totalInverseMass += rhsParticleComponent->m_InverseMass;
 		}
-		// both objects are immovable
-		if (totalInverseMass <= 0)
-			return;
-
-		const Real impulse = deltaSeperatingVelocity / totalInverseMass;
-		const Vector3 directionPostContact = contactNormal * impulse;
-		lhsParticleComponent->m_Velocity += lhsParticleComponent->m_Velocity + directionPostContact * lhsParticleComponent->m_InverseMass;
+		const Vector3 moveBasedOnTotalInverseMass = contactNormal * (-penetrationLength * totalInverseMass);
+		lhsParticleComponent->m_CurrentPosition += lhsParticleComponent->m_CurrentPosition + moveBasedOnTotalInverseMass *
+			lhsParticleComponent->m_InverseMass;
 		if (rhsParticleComponent != nullptr)
 		{
-			rhsParticleComponent->m_Velocity += rhsParticleComponent->m_Velocity + directionPostContact * 
+			rhsParticleComponent->m_CurrentPosition += rhsParticleComponent->m_CurrentPosition + moveBasedOnTotalInverseMass *
 				rhsParticleComponent->m_InverseMass;
 		}
 	}
