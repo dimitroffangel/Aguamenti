3a44beac683ca29bb1d631eb46c313038fc18978
dimitroffangel <angeldimitrov05@gmail.com>
2022-10-13 16:08:39 +0300

    moved the definition of the resolving of two colliding objects to cpp to not cluster the header

--- Physics/Systems/CollisionHelper.h
+++ Physics/Systems/CollisionHelper.h
@@ -13,32 +13,7 @@ namespace Aguamenti
 		ParticleComponent* rhsParticleComponent, const Vector3 contactNormal, const Real restitution = RESTITUTION_DEFAULT);
 	void ResolveInterpenetration(const Real deltaTime,
 		ParticleComponent* lhsParticleComponent, ParticleComponent* rhsParticleComponent, const Real penetrationLength,
-		const Vector3 contactNormal)
-	{
-		if (penetrationLength <= 0)
-		{
-			return;
-		}
-		if (lhsParticleComponent == nullptr)
-		{
-			assert(false && "Aguamenti::ResolveInterpenetration lhsParticleComponent does not exist");
-			return;
-		}
-
-		Real totalInverseMass = lhsParticleComponent->m_InverseMass;
-		if (rhsParticleComponent != nullptr)
-		{
-			totalInverseMass += rhsParticleComponent->m_InverseMass;
-		}
-		const Vector3 moveBasedOnTotalInverseMass = contactNormal * (-penetrationLength * totalInverseMass);
-		lhsParticleComponent->m_CurrentPosition += lhsParticleComponent->m_CurrentPosition + moveBasedOnTotalInverseMass *
-			lhsParticleComponent->m_InverseMass;
-		if (rhsParticleComponent != nullptr)
-		{
-			rhsParticleComponent->m_CurrentPosition += rhsParticleComponent->m_CurrentPosition + moveBasedOnTotalInverseMass *
-				rhsParticleComponent->m_InverseMass;
-		}
-	}
+		const Vector3 contactNormal);
 }
 
 
