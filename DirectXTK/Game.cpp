72bd1e7f812782058ad217ab10ea66c48ce68c3f
dimitroffangel <angeldimitrov05@gmail.com>
2022-10-13 12:32:10 +0300

    removed unused files

--- DirectXTK/Game.cpp
+++ DirectXTK/Game.cpp
@@ -23,7 +23,7 @@ Game::Game() noexcept(false)
     //   Add DX::DeviceResources::c_EnableHDR for HDR10 display.
     m_DeviceResources->RegisterDeviceNotify(this);
 
-    m_PhysicsScene = std::make_unique<BulletScene>();
+    m_PhysicsScene = std::make_unique<FireworkScene>();
     m_PhysicsScene->AddGravitationalForce(Aguamenti::Vector3(0, -0.0002f, 0));
     m_PhysicsScene->AddDragForce(0.05f, 0.01f);
 }
--- RelativeForceComponent.cpp
+++
@@ -1,7 +0,0 @@
-#include <pch.h>
-#include <Physics/Components/RelativeForceComponent>
-
-void Aguamenti::RelativeForceComponent::Update(const Real deltaTime)
-{
-
-}
